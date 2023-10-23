5548dba78c995a62ce028e4bc6ebe93bb2c8cfb3
Oliver <oliverwang008@gmail.com>
2023-10-23 12:38:01 +1100

    REACT commit for social links

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -1,47 +1,45 @@
-import './PlacesLived.css';
-import HK from '../../images/hk.png';
-import SG from '../../images/sg.png';
-import BC from '../../images/bc.png';
-import SYD from '../../images/syd.png';
-import SF from '../../images/sf.png';
-import MEL from '../../images/mel.png';
+import './SocialLinks.css';
+import FACEBOOK from '../../images/facebook.png';
+import INSTAGRAM from '../../images/instagram.png';
+import LINKEDIN from '../../images/linkedin.png';
 
-function PlacesLived() {
+
+function SocialLinks() {
     return (
 
         <div class="content2">
             <div class="place1">
                 <a href="https://en.wikipedia.org/wiki/Hong_Kong" target="_blank" rel="noreferrer">
-                    <img alt="hk" className="citypic" src={HK} />
+                    <img alt="hk" className="citypic"/>
                 </a>
             </div>
             <div class="place2">
                 <a href="https://en.wikipedia.org/wiki/Singapore" target="_blank" rel="noreferrer">
-                    <img alt="sg" className="citypic" src={SG} />
+                    <img alt="sg" className="citypic"/>
                 </a>
             </div>
             <div class="place3">
                 <a href="https://en.wikipedia.org/wiki/Barcelona" target="_blank" rel="noreferrer">
-                    <img alt="bc" className="citypic" src={BC} />
+                    <img alt="bc" className="citypic"/>
                 </a>
             </div>
             <div class="place4">
                 <a href="https://en.wikipedia.org/wiki/Sydney" target="_blank" rel="noreferrer">
-                    <img alt="syd" className="citypic" src={SYD} />
+                    <img alt="syd" className="citypic"/>
                 </a>
             </div>
             <div class="place5">
                 <a href="https://en.wikipedia.org/wiki/San_Francisco" target="_blank" rel="noreferrer">
-                    <img alt="sf" className="citypic" src={SF} />
+                    <img alt="sf" className="citypic"/>
                 </a>
             </div>
             <div class="place6">
                 <a href="https://en.wikipedia.org/wiki/Melbourne" target="_blank" rel="noreferrer">
-                    <img alt="mel" className="citypic" src={MEL} />
+                    <img alt="mel" className="citypic"/>
                 </a>
             </div>
         </div>
     )
 }
 
-export default PlacesLived;
+export default SocialLinks;
